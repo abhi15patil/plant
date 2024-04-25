9c29c95d5dbf487ec3b05e0e169861c697d8deb0
abhi15patil <98214689+abhi15patil@users.noreply.github.com>
2024-04-25 13:23:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template, request
  
 import numpy as np
 import os
-
-from keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import load_img
+`from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
 from keras.models import load_model
  
